355adb22b186ef211296be8e8a18155cec05ef1c
Daniel Rojas <rojashdezda@gmail.com>
2025-01-30 11:14:29 +0000

    Mi segundo commit

---
+++ src/gitprueba/SegundoCommit.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gitprueba;
+
+public class SegundoCommit {
+
+    public static void main(String[] args) {
+        System.out.println("Este es mi segundo commit");
+    }
+    
+}
